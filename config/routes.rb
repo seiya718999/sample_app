a3c8641ab52bfdad82b9b5b20de47c940517d42b
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-07-11 03:05:46 +0000

    [finish]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/edit'
   get '/top' => 'homes#top'
   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 end
